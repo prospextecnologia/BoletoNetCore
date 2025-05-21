82ee0f08091280931bc6ccc01e5e40667ae116af
Wanderson Alves dos Santos <wanderson@prospexti.com.br>
2025-05-21 18:43:20 -0300

    Ok.

--- BoletoNetCore/Banco/Caixa/BancoCaixa.cs
+++ BoletoNetCore/Banco/Caixa/BancoCaixa.cs
@@ -22,7 +22,7 @@ namespace BoletoNetCore
             if (!CarteiraFactory<BancoCaixa>.CarteiraEstaImplementada(contaBancaria.CarteiraComVariacaoPadrao))
                 throw BoletoNetCoreException.CarteiraNaoImplementada(contaBancaria.CarteiraComVariacaoPadrao);
 
-            contaBancaria.FormatarDados("EM TODA A REDE BANCÁRIA E SEUS CORRESPONDENTES ATÉ O VALOR LIMITE", "", "SAC CAIXA: 0800 726 0101 (informações, reclamações, sugestões e elogios)<br>Para pessoas com deficiência auditiva ou de fala: 0800 726 2492<br>Ouvidoria: 0800 725 7474<br>caixa.gov.br<br>", 6);
+            contaBancaria.FormatarDados("EM TODA A REDE BANCÁRIA E SEUS CORRESPONDENTES ATÉ O VALOR LIMITE", "", "SAC CAIXA: 0800 726 0101 (informações, reclamações, sugestões e elogios)<br>Para pessoas com deficiência auditiva ou de fala: 0800 726 2492<br>Ouvidoria: 0800 725 7474<br>caixa.gov.br<br>", 12);
 
             var codigoBeneficiario = Beneficiario.Codigo;
             if (codigoBeneficiario.Length <= 6)
